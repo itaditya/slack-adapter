3d95842fa31ef38798c42a398e76ddb5531ce31b
Brandon Keepers <bkeepers@github.com>
2018-03-21 15:42:14 -0500

    Async respons if request takes too long

--- test/index.test.js
+++ test/index.test.js
@@ -39,10 +39,23 @@ describe('commands', () => {
       .expect(200, '"hello world"')
   })
 
+  test('passes args', async () => {
+    const action = jest.fn()
+    robot.commands.register({name: 'test', action})
+
+    const command = fixture({command: '/test', text: 'hello world'})
+    await request.post('/slack/command').use(slackbot).send(command).expect(200)
+
+    expect(action).toHaveBeenCalledWith(expect.objectContaining({
+      name: 'test',
+      args: ['hello', 'world'],
+      context: expect.objectContaining({team_id: 'T0001'})
+    }), expect.any(Function))
+  })
+
   test('does not call command with invalid token', async () => {
     const command = fixture({command: '/echo', token: 'invalid'})
-    await request.post('/slack/command').use(slackbot).send(command)
-      .expect(500)
+    await request.post('/slack/command').use(slackbot).send(command).expect(500)
   })
 
   test('allows responding with command.respond', async () => {
@@ -58,8 +71,30 @@ describe('commands', () => {
       .expect(200, '"responded"')
   })
 
+  test.only('responds with 200 before timeout', async (done) => {
+    robot.commands.register({
+      name: 'delay',
+      action (command) {
+        return new Promise((resolve) => {
+          setTimeout(() => resolve({text: 'real response'}), 2501)
+        })
+      }
+    })
+
+    nock('https://hooks.slack.com')
+      .post('/commands/1234/5678', { text: 'real response' })
+      .reply(200, 'ok')
+
+    const command = fixture({command: '/delay'})
+
+    await request.post('/slack/command').use(slackbot).send(command)
+      .expect(200, {response_type: 'in_channel'})
+
+    // Wait a little longer for async response to be sent
+    // FIXME: figure out a cleaner way to do this
+    setTimeout(done, 100)
+  })
+
   // TODO
   // - error handling
-  // - response_url
-  // - timeout
 })
